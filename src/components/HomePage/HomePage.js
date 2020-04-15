30ccdf841f4481ee7ddb64f71dcdb79a1d607ca4
Jace P. Gold <jacegold@gmail.com>
2020-04-15 15:27:34 -0600

    Add navigation

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,37 @@
-import React, { Fragment } from 'react';
-import { Header } from 'semantic-ui-react';
+import React from 'react';
+import { Container, Menu, Image, Dropdown, Divider, } from 'semantic-ui-react';
 
 const HomePage = () => (
-  <Fragment>
-    <Header as='h1'>Hello awesome students!</Header>
-  </Fragment>
+  <div>
+    <Menu fixed='top' inverted>
+      <Container>
+        <Menu.Item as='a' header>
+          <Image size='mini' src='https://react.semantic-ui.com/logo.png' style={{ marginRight: '1.5em' }} />
+          Jace Gold
+        </Menu.Item>
+        <Menu.Item as='a'>Home</Menu.Item>
+
+        <Dropdown item simple text='Dropdown'>
+          <Dropdown.Menu>
+            <Dropdown.Item>List Item 1</Dropdown.Item>
+            <Dropdown.Item>List Item 2</Dropdown.Item>
+
+            <Dropdown.Divider />
+
+            <Dropdown.Item>
+              <i className='dropdown icon' />
+              <span className='text'>Submenu</span>
+              <Dropdown.Menu>
+                <Dropdown.Item>List Item 1</Dropdown.Item>
+                <Dropdown.Item>List Item 2</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown.Item>
+            <Dropdown.Item>List Item 3</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Container>
+    </Menu>
+  </div>
 );
 
 export default HomePage;
