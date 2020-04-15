9adb96cfe48ebeb4d09dc357a60be9ba0282d39a
Jace P. Gold <jacegold@gmail.com>
2020-04-15 14:52:31 -0600

    Init Commit

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import { Header } from 'semantic-ui-react';
+
+const HomePage = () => (
+  <Fragment>
+    <Header as='h1'>Hello awesome students!</Header>
+  </Fragment>
+);
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css';
+import HomePage from './components/HomePage/HomePage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>,
   document.getElementById('root')
 );
