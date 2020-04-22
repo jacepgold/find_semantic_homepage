295efbb26ead7509aafbf143777797c517f74a22
Jace P. Gold <jacegold@gmail.com>
2020-04-22 15:52:24 -0600

    Add routing and about page

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
 // Static elements
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 // Pages
-import HomePage from './components/HomePage/HomePage';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
-    <HomePage />
+    <Router>
+      <Switch>
+        <Route exact path='/' component={ HomePage } />
+        <Route exact path='/about' component={ AboutPage } />
+      </Switch>
+    </Router>    
     <Footer />
   </React.StrictMode>,
   document.getElementById('root')
