3f6801b25d138d1ab62c9cffdeeafccf8622baa3
Jace P. Gold <jacegold@gmail.com>
2020-04-29 16:05:29 -0600

    Gallery page filled, header stuff removed

--- src/components/GalleryPage.js
+++ src/components/GalleryPage.js
@@ -1,51 +1,59 @@
 import React from 'react';
-import { Container, Grid, Image, } from 'semantic-ui-react';
+import { Container, 
+  Grid, 
+  Image, 
+  Segment, 
+  Header, 
+} from 'semantic-ui-react';
 
 const GalleryPage = () => (
-  <Container>
-    <Grid>
+  <Container style={{ marginTop: '7em' }}>
+    <Segment>
+      <Header as='h2' textAlign='center'>Gallery</Header>
+    </Segment>
+    <Grid stackable>
       <Grid.Row columns={3}>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <Image src='https://bit.ly/2KLToTW' />
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <Image src='https://bit.ly/2KLToTW' />
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <Image src='https://bit.ly/2KLToTW' />
         </Grid.Column>
       </Grid.Row>
 
       <Grid.Row columns={4}>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <Image src='https://bit.ly/2yRcBAT' />
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <Image src='https://bit.ly/2yRcBAT' />
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <Image src='https://bit.ly/2yRcBAT' />
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <Image src='https://bit.ly/2yRcBAT' />
         </Grid.Column>
       </Grid.Row>
 
       <Grid.Row columns={5}>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <Image src='https://bit.ly/2KOuSBe' />
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <Image src='https://bit.ly/2KOuSBe' />
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <Image src='https://bit.ly/2KOuSBe' />
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <Image src='https://bit.ly/2KOuSBe' />
         </Grid.Column>
         <Grid.Column>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+          <Image src='https://bit.ly/2KOuSBe' />
         </Grid.Column>
       </Grid.Row>
     </Grid>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   Menu,
   Container,
-  Dropdown,
   Image,
 } from 'semantic-ui-react';
 import { Link, } from 'react-router-dom';
@@ -33,37 +32,6 @@ const Header = () => (
           Gallery
         </Link>
       </Menu.Item>
-
-      <Dropdown item simple text='Dropdown'>
-        <Dropdown.Menu>
-          <Dropdown.Item>
-            <Link to='/'>
-              Home
-            </Link>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <Link to='/about'>
-              About
-            </Link>
-          </Dropdown.Item>
-
-          <Dropdown.Divider />
-
-          <Dropdown.Item>
-            <i className='dropdown icon' />
-            <span className='text'>More</span>
-            <Dropdown.Menu>
-              <Dropdown.Item>
-                <Link to='/gallery'>
-                  Gallery
-                </Link>
-              </Dropdown.Item>
-              <Dropdown.Item>List Item 2</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown.Item>
-          <Dropdown.Item>List Item 3</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
     </Container>
   </Menu>
 );
